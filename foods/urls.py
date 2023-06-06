0faf2607ea52c0b20a99ec5655a0c62e92f06010
Игорь Манихин <i.manikhin@vk.team>
2023-06-06 19:30:36 +0300

    Implemented food category list view

---
+++ foods/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from foods import views
+
+urlpatterns = [
+    path("foods/", views.FoodCategoryListView.as_view())
+]
--- foods/views.py
+++ foods/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.db.models import Exists
+from django.db.models import OuterRef
+from django.db.models import Prefetch
+from rest_framework import generics
 
-# Create your views here.
+from foods import models
+from foods import serializers
+
+
+def _get_food_categories_queryset():
+    prefetch_foods_subquery = models.Food.objects.filter(is_publish=True).all()
+    exists_foods_subquery = Exists(
+        models.Food.objects.filter(is_publish=True, category=OuterRef('pk')).all()
+    )
+
+    return models.FoodCategory.objects.prefetch_related(
+        Prefetch("food", queryset=prefetch_foods_subquery), "food__additional",
+    ).annotate(foods_exist=exists_foods_subquery).filter(foods_exist=True).order_by().all()
+
+
+class FoodCategoryListView(generics.ListAPIView):
+    queryset = _get_food_categories_queryset()
+    serializer_class = serializers.FoodListSerializer
